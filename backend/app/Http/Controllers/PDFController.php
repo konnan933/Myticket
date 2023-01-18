19d9289917830cc9af750858c7066173f0321a1a
konnan933 <95681490+konnan933@users.noreply.github.com>
2023-01-18 23:19:30 +0100

    qrcode hashelt azonosítot, és bladebe megjelenik

--- backend/app/Http/Controllers/PDFController.php
+++ backend/app/Http/Controllers/PDFController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PDFController extends Controller
 {
-    public function createPDF($qrcode)
+    public function createPDF($user, $qrcode)
     {
-
-        return view('pdf', compact('qrcode'));
+        $user_nev = User::find($user)->fel_nev;
+        $qrCodehash = Hash::make($qrcode);
+        return view('pdf', compact('qrCodehash', 'user_nev'));
     }
 }
---
+++ backend/resources/views/pdf.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>How to Generate QR Code in Laravel 9</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <div class="container mt-4">
+        <img src="/images/small_logo.png" class="rounded mx-auto d-block" alt="asd">
+        <div class="card">
+            <div class="card-header">
+                <h2>Jegy azonosítód : {{$user_nev}}</h2>
+            </div>
+            <div class="card-body">
+                {!! QrCode::size(300)->generate($qrCodehash) !!}
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
