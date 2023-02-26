0d9110434b972235f4c300a55fb5a1b075de6696
konnan933 <konnan933@gmail.com>
2023-02-26 14:29:54 +0100

    UserObserver létre hozva, logout routing fix

--- frontend/src/PageContent/navbar/components/LogoutButton.js
+++ frontend/src/PageContent/navbar/components/LogoutButton.js
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { fetchLogout } from '../../../redux/thunks/Auth';
 
 function LogoutButton() {
@@ -11,9 +11,12 @@ function LogoutButton() {
 
   const handleLogout = () => {
     dispatch(fetchLogout());
-    return <Navigate to="/" />;
   };
 
-  return <Button onClick={handleLogout}>{t('LOGOUT')}</Button>;
+  return (
+    <Link to="/">
+      <Button onClick={handleLogout}>{t('LOGOUT')}</Button>;
+    </Link>
+  );
 }
 export default LogoutButton;
