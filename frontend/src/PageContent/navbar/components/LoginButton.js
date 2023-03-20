e2c1bed88b77f239dfc71aa4d1b50a7ae16a9feb
konnan933 <95681490+konnan933@users.noreply.github.com>
2023-03-20 14:33:48 +0100

    register responsive

--- frontend/src/PageContent/navbar/components/LoginButton.js
+++ frontend/src/PageContent/navbar/components/LoginButton.js
@@ -1,15 +1,17 @@
-import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import '../underline.css';
 
 function LoginButton() {
   const { t } = useTranslation('rootes');
 
-
-  return (<Link to={'/login'}>
-                      <Button variant="outlined" sx={{ color: 'white', borderColor: 'white' }}>
-                        {t('LOGIN')}
-                      </Button>
-                    </Link>);
+  return (
+    <Link to={'/login'}>
+      <Typography className="text-white link-underline link-underline-black">
+        {t('LOGIN')}
+      </Typography>
+    </Link>
+  );
 }
 export default LoginButton;
