e9b180cfd6f8cb0db077b8327d9b438f87bc27bd
konnan933 <konnan933@gmail.com>
2023-03-23 11:41:58 +0100

    glass effect responsive fix

--- frontend/src/PageContent/Home/components/Glass.js
+++ frontend/src/PageContent/Home/components/Glass.js
@@ -1,8 +1,10 @@
 import { Button, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 function Glass() {
   const { t } = useTranslation('home');
+  const navigate = useNavigate();
   return (
     <div className=" w-full h-1/10 flex justify-center items-center">
       <div
@@ -16,11 +18,14 @@ function Glass() {
           backgroundRepeat: 'no-repeat'
         }}
         className="mb-5 rounded-2xl flex justify-evenly items-center">
-        <div className="flex justify-evenly backdrop-blur-[10px] bg-[#ffffff33]  w-2/3 h-1/2 z-10 rounded-2xl items-center p-16">
+        <div className="flex justify-evenly max-md:flex-col max-md:w-4/5  max-md:h-2/3  backdrop-blur-[10px] bg-[#ffffff33]  w-2/3 h-5/6 z-10 rounded-2xl items-center ">
           <Typography>{t('AD_EVENT_TEXT')}</Typography>
           <Button
             variant="outlined"
-            sx={{ borderColor: 'white', color: 'white', textTransform: 'none', letterSpacing: 2 }}>
+            onClick={() => {
+              navigate('/userAddEvent');
+            }}
+            sx={{ borderColor: 'white', color: 'white', textTransform: 'none', letterSpacing: 1 }}>
             {t('AD_EVENT_BUTTON')}
           </Button>
         </div>
