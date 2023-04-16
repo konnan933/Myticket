7c03bf27e19f2d505e99d71e7ac1470cb4277745
konnan933 <95681490+konnan933@users.noreply.github.com>
2023-04-16 20:26:54 +0200

    event page picture responsive

--- frontend/src/PageContent/EventContent/components/EventPicture.js
+++ frontend/src/PageContent/EventContent/components/EventPicture.js
@@ -1,17 +1,19 @@
+import { useMediaQuery } from '@mui/material';
 import event from 'API/Event';
 import { useParams } from 'react-router-dom';
 
 function EventPicture() {
   const { id } = useParams();
+  const mobile = useMediaQuery('(min-width:765px)');
 
   return (
-    <div className="w-4/5">
+    <div className="w-4/5 max-md:w-9/10">
       <div
         style={{
           backgroundImage: `url(${event.eventPicture}${id})`,
           width: '100%',
-          height: '380px',
-          backgroundSize: 'cover',
+          height: mobile ? '400px' : '350px',
+          backgroundSize: 'contain',
           backgroundPosition: 'center center',
           backgroundRepeat: 'no-repeat'
         }}
